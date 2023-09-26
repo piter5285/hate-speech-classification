9c6dceaf58e50f63b73b23a482386dfee11851df
piter5285 <piotrrybacki100@gmail.com>
2023-09-26 14:44:27 +0200

    last update

--- hate/constants/__init__.py
+++ hate/constants/__init__.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # Common constants
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 ARTIFACTS_DIR = os.path.join("artifacts", TIMESTAMP)
-BUCKET_NAME = 'hate-speech'
+BUCKET_NAME = 'hate-speech-dataset'
 ZIP_FILE_NAME = 'dataset.zip'
 LABEL = 'label'
 TWEET = 'tweet'
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="hate-speech-classification",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="piter5285",
+    author_email="piotrrybacki100@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
